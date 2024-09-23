a54a1e55d1384746ea661a3ec475e7f2248b0e1e
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-09-23 20:35:02 -0300

    first commit

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,6 +1,29 @@
+using minimal_api.Dominio.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+
+
+
+app.MapPost("/login", (LoginDTO loginDto) => {
+
+
+ if(loginDto.Email == "adm@teste.com" && loginDto.Senha == "12345678"){
+   
+   return Results.Ok("Login feito com sucesso!");
+ }
+ else{
+
+   return Results.Unauthorized();
+ }
+});
+
+
+
 app.Run();
+
+
+
